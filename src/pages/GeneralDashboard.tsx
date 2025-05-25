228969e9b4f15589899ef753b20a47576aeb6ef8
Gonçalo <goncalo.ferreira@student.uva.nl>
2025-05-25 16:12:09 +0200

    Added chart selection for 4 charts.

--- src/pages/GeneralDashboard.tsx
+++ src/pages/GeneralDashboard.tsx
@@ -35,8 +35,18 @@ export default function GeneralDashboard() {
   const [dataMap, setDataMap] = useState<Map<string, [number, string][]>>(
     new Map()
   );
-  const [selectedMetric, setSelectedMetric] = useState<string>('');
+  const [selectedMetric, setSelectedMetric] = useState<string[]>(
+    new Array(NR_CHARTS).fill('')
+  );
   const [loading, setLoading] = useState<boolean>(true);
+
+  function handleUpdateSelectedMetric(index: number, newMetric: string) {
+    setSelectedMetric(prev => {
+      const updated = [...prev];
+      updated[index] = newMetric;
+      return updated;
+    });
+  }
 
   useEffect(() => {
     setLoading(true);
@@ -49,7 +59,10 @@ export default function GeneralDashboard() {
       setDataMap(results);
       const keys = Array.from(results.keys());
       setMetrics(keys);
-      setSelectedMetric(keys[0] || '');
+
+      for (let i = 0; i < NR_CHARTS; i++) {
+        handleUpdateSelectedMetric(i, keys[i] || '');
+      }
       setLoading(false);
     });
   }, []);
@@ -58,15 +71,17 @@ export default function GeneralDashboard() {
   for (let i = 0; i < NR_CHARTS; i++) {
     Charts.push(
       <Grid2 size={{ xs: 12, sm: 6 }}>
-        <Paper elevation={3} sx={{ p: 2, width: '100%', m: 2 }}>
+        <Paper elevation={3} sx={{ p: 2, width: '100%' }}>
           <MetricSelector
-            selectedMetric={selectedMetric}
-            setSelectedMetric={setSelectedMetric}
+            selectedMetric={selectedMetric[i]}
+            setSelectedMetric={newMetric =>
+              handleUpdateSelectedMetric(i, newMetric)
+            }
             metrics={metrics}
           />
           <ScaphChart
             key={`${selectedMetric}-${i}`}
-            rawData={dataMap.get(selectedMetric) || []}
+            rawData={dataMap.get(selectedMetric[i]) || []}
           />
         </Paper>
       </Grid2>
@@ -77,14 +92,19 @@ export default function GeneralDashboard() {
     <div style={styles.main}>
       <Paper
         key="grid-element-main"
-        style={{ ...styles.grid, flexDirection: 'column', minWidth: '100%' }}
+        style={{
+          ...styles.grid,
+          flexDirection: 'column',
+          minWidth: '100%',
+          minHeight: '300px'
+        }}
         elevation={3}
       >
         {loading ? (
           <CircularProgress />
         ) : (
           <Grid2 sx={{ width: '100%', height: '100%' }}>
-            <Grid2 sx={styles.chartsWrapper}>{Charts}</Grid2>
+            <Grid2 sx={{ ...styles.chartsWrapper }}>{Charts}</Grid2>
           </Grid2>
         )}
       </Paper>
