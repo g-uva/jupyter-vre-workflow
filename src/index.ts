a96730ad99d7bf3d2c87aae3495b229994c17ae5
Gonçalo <goncalo.ferreira@student.uva.nl>
2025-05-26 21:48:24 +0200

    Dashboard opening by default.

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
     // Ensure the tracker is restored properly on refresh
     restorer.restore(tracker, {
       command: `${namespaceId}:open`,
-      name: () => 'greendigit-jupyterlab'
+      name: () => 'gd-ecojupyter'
       // when: app.restored, // Ensure restorer waits for the app to be fully restored
     });
 
@@ -52,8 +52,8 @@ const plugin: JupyterFrontEndPlugin<void> = {
     const newWidget = async (): Promise<MainAreaWidget<MainWidget>> => {
       const content = new MainWidget();
       const widget = new MainAreaWidget({ content });
-      widget.id = 'greendigit-jupyterlab';
-      widget.title.label = 'GreenDIGIT Dashboard';
+      widget.id = 'gd-ecojupyter';
+      widget.title.label = 'GreenDIGIT EcoJupyter Dashboard';
       widget.title.closable = true;
       return widget;
     };
@@ -61,20 +61,27 @@ const plugin: JupyterFrontEndPlugin<void> = {
     // Add an application command
     const openCommand: string = `${namespaceId}:open`;
 
+    async function addNewWidget(
+      shell: JupyterFrontEnd.IShell,
+      widget: MainAreaWidget<MainWidget> | null
+    ) {
+      // If the widget is not provided or is disposed, create a new one
+      if (!widget || widget.isDisposed) {
+        widget = await newWidget();
+        // Add the widget to the tracker and shell
+        tracker.add(widget);
+        shell.add(widget, 'main');
+      }
+      if (!widget.isAttached) {
+        shell.add(widget, 'main');
+      }
+      shell.activateById(widget.id);
+    }
+
     app.commands.addCommand(openCommand, {
       label: 'Open GreenDIGIT Dashboard',
       execute: async () => {
-        let widget = tracker.currentWidget;
-        if (!widget || widget.isDisposed) {
-          widget = await newWidget();
-          // Add the widget to the tracker and shell
-          tracker.add(widget);
-          shell.add(widget, 'main');
-        }
-        if (!widget.isAttached) {
-          shell.add(widget, 'main');
-        }
-        shell.activateById(widget.id);
+        addNewWidget(shell, tracker.currentWidget);
       }
     });
 
@@ -86,6 +93,13 @@ const plugin: JupyterFrontEndPlugin<void> = {
       const widget = await newWidget();
       tracker.add(widget);
       shell.add(widget, 'main');
+    }
+
+    const seenKey = 'greendigit-jupyterlab-seen';
+    const seen = window.sessionStorage.getItem(seenKey);
+
+    if (seen) {
+      addNewWidget(shell, tracker.currentWidget);
     }
   }
 };
