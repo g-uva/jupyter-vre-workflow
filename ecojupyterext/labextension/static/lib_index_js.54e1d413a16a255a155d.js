35f74d0abde7fc60987fd8bb72871b16b886af49
Gonçalo <goncalo.ferreira@student.uva.nl>
2025-05-26 19:12:54 +0200

    Changing config files.

---
+++ ecojupyterext/labextension/static/lib_index_js.54e1d413a16a255a155d.js
@@ -0,0 +1,31 @@
+"use strict";
+(self["webpackChunkecojupyterext"] = self["webpackChunkecojupyterext"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/**
+ * Initialization data for the ecojupyterext extension.
+ */
+const plugin = {
+    id: 'ecojupyterext:plugin',
+    description: 'A JupyterLab extension for sustainability metrics.',
+    autoStart: true,
+    activate: (app) => {
+        console.log('JupyterLab extension ecojupyterext is activated!');
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.54e1d413a16a255a155d.js.map
